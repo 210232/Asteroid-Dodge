57166c737be9d110fbcae6df1dd131ed778bfb30
210232 <210232@hkis.edu.hk>
2016-05-19 10:09:28 +0800

    MADE THE FROGGEN CHARACTER MOVE

--- gamedev.js
+++ gamedev.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
 
-//Your jquery goes here
+var main = $("#sprite1");
 
+$(document).keydown(function(e) {
+  $(main).keydown;
+  switch(e.which) {
+  
+  //move left
+  case 37:
+    $(main).animate({left: "-=40px"}, 'fast');
+    break;
+ //move right
+  case 39:
+    $(main).animate({left: "+=40px"}, 'fast');
+    break;
+  };
+});
 });
