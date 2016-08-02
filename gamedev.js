ab7daab17b9fccb4d49995069b23449de0dc6b48
Ian <ianfchow@me.com>
2016-08-02 14:36:55 +0800

    Remove unnecessary redefinitions of "collision" function

--- gamedev.js
+++ gamedev.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-  var main = $("#sprite1"), asteroid = $(".asteroid"), asteroid2 = $(".asteroid2"), score = 0;
+  var main = $("#sprite1"),
+    asteroid = $(".asteroid"),
+    asteroid2 = $(".asteroid2"),
+    score = 0;
 
   $(document).keydown(function (e) {
-    $(main).keydown;
+    $(main).keydown; // NOTE: You mean $(main).keydown()? (Method Call)
     switch(e.which) {
       //move left
       case 37:
@@ -69,29 +72,6 @@ $(document).ready(function () {
       }
     });
   });
-  function collision($div1, $div2) {
-    var x1 = $div1.offset().left;
-    var y1 = $div1.offset().top;
-    var h1 = $div1.outerHeight(true);
-    var w1 = $div1.outerWidth(true);
-    var b1 = y1 + h1;
-    var r1 = x1 + w1;
-    var x2 = $div2.offset().left;
-    var y2 = $div2.offset().top;
-    var h2 = $div2.outerHeight(true);
-    var w2 = $div2.outerWidth(true);
-    var b2 = y2 + h2;
-    var r2 = x2 + w2;
-    //below is an if statement - if the variables calculate to the right formula, it will return true or false
-    
-    // Original
-    /*
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-      return true;
-    */
-    // Refactored
-    return !(b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2);
-  }
 
   window.setInterval(function() {
     //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
@@ -107,30 +87,7 @@ $(document).ready(function () {
       }
     });
   });
-  function collision($div1, $div2) {
-    var x1 = $div1.offset().left;
-    var y1 = $div1.offset().top;
-    var h1 = $div1.outerHeight(true);
-    var w1 = $div1.outerWidth(true);
-    var b1 = y1 + h1;
-    var r1 = x1 + w1;
-    var x2 = $div2.offset().left;
-    var y2 = $div2.offset().top;
-    var h2 = $div2.outerHeight(true);
-    var w2 = $div2.outerWidth(true);
-    var b2 = y2 + h2;
-    var r2 = x2 + w2;
-    //below is an if statement - if the variables calculate to the right formula, it will return true or false
-    
-    // Original
-    /*
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-      return true;
-    */
-    // Refactored
-    return !(b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2);
-  }
-
+  
   window.setInterval(function() {
     //function that makes the magic happen! Below, jQuery prints the word "FALSE" into #results
     //IMPORTANT!!! Below declares the class of divs that your sprite collides with!!
